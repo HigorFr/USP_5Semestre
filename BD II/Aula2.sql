463f6a19d5e125cdb37abb2e83d8c9dc4e60cef9
Higor <higor.freitas@usp.br>
2026-03-02 20:36:08 -0300

    BD I

---
+++ BD II/Aula2.sql
@@ -0,0 +1,42 @@
+#Aula 2
+    #Soluções híbridas de SQL e NoSQL
+    #Desafio de transição entre camadas, maior conhecimento
+
+
+#Abstração de código, facilitar construção e separar em módulo
+    #"Considerar que tal parte funciona"
+    #Linguagem sem código
+
+
+#Logíco é a organização como vizualizada pelo usuário
+#Físico é a organização como armazenada no sistema, eletronicamente
+
+#Método de Arquivo
+    #Organização em arquivos tem mutias redundancias com relação à indices do arquivo, e com pouca relação com o dado em cada arquivo em sí
+    #Muita manutenção, dificuldade em desenvolvimento em aplicações
+
+#Evolução
+    #Dados e Programas juntos
+    #programa e Arquivos
+    #Depois  programas -> SGBD -> Dados
+        #SGBD é como uma interface, que gerencia dados no banco, um programa, produto
+        #Cada fabricante terá suas propriedades
+        #Fica entre SO e Aplicação
+
+
+#Camdas SGBD
+    #Camada de Aplicação: Interface, linguagem de consulta, etc
+    #Camada Lógica: Gerenciamento de dados, controle de concorrência, etc
+    #Camada Física: Armazenamento, organização em disco, etc
+
+
+#Muitos tipos de classificações de BD, evolução não linear
+#Muitos tipos de representação de dados, inclusive pedras seria.
+
+
+#Represntação modleo relacional é boa, pela base matémática de teoria dos conjuntos, muito bem embasada.
+
+#SQL parcialmente padronizado, porque cada fabricante criou o seu.
+
+#Cada modelo tem suas vantagens e desvantagens, e cada um tem um nicho de mercado, não tem um melhor que o outro, depende do uso.
+
--- IA/Aula2.py
+++ IA/Aula2.py
@@ -9,3 +9,9 @@
 #LMM, associação como papagaios usando estócastica estatística. É a crtítica
     #Não são concientes
 
+#A IA seria como um método para evitar a relação exponencial do aprendizado, usando o conhecimento e experiência, se fosse apenas decorar seria impossível, pois a explosão exponencial tornaria impossível.
+    #Ou seja, é evitar decorar
+
+
+
+#
