e8690523dec24e20eeffda8b9f43763a79b47abb
Higor <higor.freitas@usp.br>
2026-03-02 22:09:50 -0300

    IHC

--- IHC/Aula2.py
+++ IHC/Aula2.py
@@ -1,1 +1,68 @@
-#
+#Aula 2
+
+#Perspectiva histórica
+
+
+#Inicio da computação ocorreu a crise do software
+    #Nova área, sem mutias boas práticas definidas, nem sistematizados
+
+#EM 1968 foi realizada a primeira conferência sobre o assunto, onde foi discutida a crise do software e a necessidade de melhores práticas de desenvolvimento.
+    #Criação de metrologias, nome "Engenharia de Software", sistematizado como na engenharia tradicional, com processos, metodologias, ferramentas, etc.
+    #Premissa de previsibilidade, antecipação de detalhes, como construir uma casa. Isso foi a base para esse pensamento.
+    #Ele era para pensar no projeto como um todo, mas na prática, isso não funcionou muito bem, pois o software é algo mais complexo e dinâmico do que a construção de uma casa.
+
+
+#"Com a documentação, qualuqer um pode desenolver", Hoje em dia não se entende mais assim, o engenheiro faz parte do processo, ele tem um papel importante, não é apenas seguir um processo, mas também tomar decisões, resolver problemas durante o código.
+    #Software é mais volátil, ele muda depois de pronto, as requisições não tem como ser definidas de antemão
+
+#Modelo de Prototipação
+    #Ciclo, Requisitos, Elaborar projeto, construir protótipo, avaliar, refinar, obter mais requisitos, repetir.
+    #Mas tinha a premissa de "Ok, temos os requisitos, vamos começar do zero agora com todos requisitos"
+    #Mas na prática, os clientes não sabiam o diferenciar o produto do protótipo, e o cliente botava pressão para versão final ficar pronta instantaneamente, o que não era possível, e isso gerava frustração e pressão.
+
+#Tem outros modelos, como o Incremental, Espiral e Cascata.
+    #No geral, prototipação assim como todos os outros tinham muito retrabalho, cada requisição tinha DIgrama de classe, modelo de algoritimo etc... e etc..., documentação ficava obsoleta, implementação mudava muito, e isso gerava muito retrabalho, e o cliente não tinha como acompanhar isso, e isso gerava frustração.
+
+#Com tudo isso, surgiu o manifesto ágil entre 17 gurus da comunidade dev para tentar resolver esses problemas, e eles criaram o manifesto ágil, que tem 4 valores e 12 princípios.
+    #Tinha uma percepção ruim inicial pelos tradicionalista "Voltaria ao caos"
+    #Principios que valorizavam mais a comunicação e não o contrato
+    #Surgiram dele os méotods ágeis, como o Scrum, XP, Kanban, etc... que são mais flexíveis e adaptáveis às mudanças, e valorizam mais a comunicação e colaboração entre os membros da equipe e com o cliente.
+    #Devem ser caracterizadas pelo o que tem, e não pelo que não tem, chamado de adhoc ou go on horse
+
+#Conceiro de criar um MVP, que é funcional, pensado para o final, diferente do protótipo.
+#Métodos são bem vindos para agregar valor
+#Espaços curto de tempo, com feedback rápido e funcional em tempo diário, (Sem perfer tempo documentando muito)
+#Sustentável, ritmo de trabalho sustentável, com constância.
+    #Inclusive a parte humana no gera é bem definida no méotodo ágil.
+#Foco no essencial, não antecipar funcionalidades pois mudar.
+    #Ou seja, não muda classe, nem arquitetura porque "Pode ser que algo seja adicionado", mas sim, pensar no que é essencial para o produto agora. Na hora de mudar você estima e refatora o que for necessário
+    
+#==============
+
+#Conceitos fundamentais de IHC
+    #Pessoas comuns não sabem o que tem por tras de uma interface, é o sistema inteiro para elas. Por isso a interface é muito improtante para experiência do usuário, pois é aquilo que agrega valor para ele.
+
+    #Interação - É o uso da interface, o que o usuário faz para usar a interface, como ele interage com ela.
+        #Possui elementos, lexicos, sintáticos e semânticos.
+            #Lexicos - São os elementos básicos da interface, como botões, campos de texto, etc...
+            #Sintáticos - São as ações de como
+            #Semanticos -
+
+
+#A interação é uma evolução
+    #Formula uma intenção do que se quer
+    #Planeja sua ação
+    #Atuo sobre a interface
+    #Percebe e interpreta o resultado 
+    #Avalia o resultado.
+
+#Affrodance
+    #Affrodance é a propriedade de um objeto que indica como ele pode ser usado, ou seja, quais ações ele permite. Por exemplo, um botão tem a affrodance de ser clicado, uma alavanca tem a affrodance de ser puxada, etc... A affrodance é importante para a usabilidade da interface, pois ajuda o usuário a entender como interagir com ela.
+    #Basciamente a intuitivade dele
+
+    #Modelo Mental
+        #Modleo mental é o que você cria na sua cabeça quando você vai interagir com algo difernete a partir da sua experiência previa, pode estar certo ou errado.
+
+    #Siga convenções
+    #Não torne simples complexo
+    #Coisas complexas, usem texto
