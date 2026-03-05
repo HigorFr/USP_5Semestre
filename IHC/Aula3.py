d0a55ed52346063d4830e6cbea0d71e958f9eed8
Higor <higor.freitas@usp.br>
2026-03-05 20:16:32 -0300

    IHC

---
+++ IHC/Aula3.py
@@ -0,0 +1,64 @@
+#Aula 3
+
+#Cognição
+#Cogniação é o processo de aprender, e elaborar transmissão de conhecimento
+#Ela pode ser dividida em muitos aspectos, fala, leitura, escrita, etc
+
+#Norman distingue em 2 tipos de coginiação
+    #Experimental é aquilo que já é feito intuitivamente, sem muita carga cognitiva. Como dirigir um carro, ler etc...
+    #Inclusive, porcessos assim, muito naturais muitas vezes nem geram informação, nem geram memória.
+    #Esse é o objetivo de interação com usuário em um sistema de informação
+
+    #Reflexiva é aquela que exige mais atenção, mais esforço cognitivo, como aprender a dirigir um carro, ou aprender a ler.
+    #Ou resolver um problema mas em um contexto diferente
+
+    #Da para ver também como um pensamento rápido e um lento
+
+
+#Percepção
+    #É o processo de interpretar os estímulos sensoriais, e atribuir significado a eles, como audição, visão, tato, etc
+    #Visão é o mais predomidante para pessoas videntes (Pessoas que enxergam), seguido pela audição e tato
+    #Ela também envolve outros processos cognitivos ocmo méoria, atençãoe linguagem
+    #Percepção não é pura, não é real, ela é influenciada por fatores, passado (Experiencias), Presente (Contexto) e Futuro (Objetivos)
+        #Ou seja, nem todo usuário percebe a mesma coisa em uma tela, isso pode inclusive atrapalha
+        
+
+    #Fatores que afetam
+        #Percepção dirigida, viéses ditos, informação parcial sobre o conteúdo que está sendo percebido que influencia sua percepção
+        
+        #Padrões ou estruturas familiares, assumir certas informações que se ela ja de fato existirem com base no contexto (VOcê sempre vai lebrar de uma cozinha com fogão, mesmo que ela não tenha um)
+
+        #Habituação, é basicamente uma adapação, um senso comum formado, integrado ao processo. Um ruído por exemplo é acostumado.
+
+        #Piscar atencional, basicamente sempre quando há um estimo que de fato afeta realmente a percepção, a percepção seguinte será prejudicada, ou seja, se perde a atenção por conta d eoutra mais interessnate
+
+
+#Cegueira a mudanças
+    #Usuarios não percebem mudamças na interface por conta de paradigmas na estrutura, mesmo quando grende
+    
+
+#Cegueira a banner
+    #Usuarios não percebem banners, mesmo quando eles estão em um local de destaque, por conta de uma adaptação, ou seja, eles se acostumam a ignorar aquilo, mesmo que seja importante
+
+
+
+#Percepção enviesada pelo contexto atual
+    #A percepção será definida pelo contexto a sua volta, exemplo do Polish silverware, pode ser Polonês ou Polir.
+
+#Percepção enviesada pelos objetivos
+    #Existe um filtro das percepções, e com base naquilo que procuramos, aquilo chega mais facilmente
+    #Esse filtro é útil para manter a sanidade e nossa sobrevivência, existe certos objetivos primordiais, como sobreviver
+
+
+
+#Implicações para desing
+    #Separação, botãos, contrates bom, estimulos visuais etc...
+
+
+#Atenção Multitarefa
+    #Existe uma certa construção para gerar atenção, ficar trocando essa construção para diferentes tipos de contexto é cansativo, e tem impacto cognitivo
+    #Por isso é necessário saber quais informações são importantes para seu usuário, evitar poluir muitas informações
+
+
+
+
