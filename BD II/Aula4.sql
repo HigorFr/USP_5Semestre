ee0de5628c4c85be7af9a0906ea950e9dcefd732
Higor <higor.freitas@usp.br>
2026-03-09 21:57:23 -0300

    IHC

---
+++ BD II/Aula4.sql
@@ -0,0 +1,15 @@
+#Aula 4
+    
+#Avanço com relação ao modelo relacional
+#Limites
+
+    #Adaptação de dimensionalidade
+    #Especificadades do mundo não especficadas
+    #Impossível rempresentar.
+
+#Oturas representações
+    #Cardinalidade usando bolinha e quantidade de linhas para uma caixa.
+    
+
+
+
---
+++ IHC/Aula4.py
@@ -0,0 +1,77 @@
+#Interpetação
+
+
+#Varios modos de interpretar uma mesma interface por diferentes aspectos
+
+
+#Proximidade
+    #Distancia entre objetos afeta a percepção de agrupamento, objetos próximos são percebidos como um grupo, relacionados
+
+#Similaridade
+    #Elementos semelhantes são percebidos como parte de um grupo também, como cor, forma, tamanho, etc
+    #Exemplo, emails em negrito são do grupo "Não lidos", e os normais são do grupo "Lidos"
+
+
+#Continuidade
+    #Elementos alinhados ou organizados em uma linha ou curva que reaparece parcialmente são entendidos como parte de um mesmo
+
+
+
+#Fechaemnto
+    #Partes incompletas tendem a ser percebidas como um todo, mesmo que haja lacunas
+
+#Simetria
+    #Estruturas compelxas tendem a ser quebradas em menores para haver um entendimento melhor
+
+
+#Figura e fundo
+    #Elementos são percebidos como figura (Foco) ou fundo (Contexto), e isso pode ser influenciado por fatores como cor, contraste, etc
+    #Coisas maiores normalmente são o fundo
+
+
+
+#Distinto Comum
+    #Meio não usado, basicamente uma diferença que torna todos similiares dado um contexto
+
+
+#Região Comum
+    #Delimitação em uma região para destacar
+
+
+#Ponto Focal
+    #Concentração imediata a um elemtno por conta de uma diferença, para chamar atenção
+
+#==================
+
+
+#Estruturação das informações (Nâo sei se os topicos estão certos)
+    #Informações estruturadas são mais fácil de entender, listas, checkboxes, tabelas
+    #Leitura de telas, usuarios não tendem a ler palavra por palavras, tem uma dinamica direta
+    #Chuncking é quebrar a informação em pedaços menores para facilitar a compreensão (Ex CPF com ponto)
+    #Percepção de cores, bordas são claras e o ser humano é bom nisso, cor afeta pela apresentação, contexto etc...
+        #Fundo e Texto, cores complementares, calculo de luminancia, contraste, etc...
+        #Não se pode usar cor como unico meio para dar significado à objetos
+    
+    #Visão foveal x Periferica, existe uma grande diferente do angulo visto central (fovial) para o visto perifericamente, reslução muda dependendo do quão cental. 
+        #Olhos tem 6 a 7 milhões de cones, na região cental ela estão muito próximas, e na periferia elas estão mais espaçadas.
+        #Informação são comprimidas pela quantidade de cones para cada neuronio na periferica
+        #Processosamento visual também é fator, o centro usa 50% e o resto os outros 50%.
+        #Se fosse tudo mesma resolução da central, o cerebro precisaria ser maior para lidar com todo esse procesamento.
+        #Ou seja, isso afeta a experiencia dele, pois coisas relacionadas devem estar na mesma Fovea
+
+    #Adendo ao principio de gestalt, coisas muitos conectadas parecem mais relacionadas, como um menu de navegação, ou um grupo de botões, ou um grupo de informações
+
+
+    #Etc...
+
+
+#============
+
+
+
+
+
+
+
+
+
