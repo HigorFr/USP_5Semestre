905057a2dddca6d99f990985bac4ce5dac9b9d5f
Higor <higor.freitas@usp.br>
2026-03-12 20:07:44 -0300

    ihc

---
+++ IHC/Aula5.py
@@ -0,0 +1,34 @@
+#Memória
+
+    #Permite o aprendizado, usando experiências passadas, também te viéses
+    #Registra o que já foi feito
+    #Existe um filtro, para quais informações vão para memória, ela não é ilimitada. E esse filtro não é perfeito nem totalmente controlável
+    #Contexto não pode mudar muito, pois isso irá entrar em conflito com a memŕoia deles. Isso é importante nas definições.
+
+
+#Curto Prazo
+    #Memória de trabalho, para manter informações ativas, ela se altera conforme o contexto, o objetivo
+    #Volátil, e limitado. Se não for ensaiada, ela pode se peder ou esquecer muito rápido
+        #Isso exige interfaces limpas, com pouco conteúdo, para não o destrair
+        #Exemplo são, calculo no papel, checklist em compras, etc...
+
+    #Isso exige uma que a interface deixe claro para o usuário o efeitos de suas ações, um resumo.
+    #Também alterar sobre tarefas incompletas, coisas faltantes, para que o usuário não saia esquecendo de fazer algo
+
+#Longo Prazo
+    #Seãnitca: Memória de fatos, conceitos, conhecimento geral, etc... Não é ligada a um contexto específico, é mais geral
+    #Episódica: Memória de eventos, experiências pessoais, ligada a um contexto específico.
+    #Procedural: Memória de habilidades, ações, como fazer algo, etc... Não é necessariamente consciente, é mais automática, como andar de bicicleta, dirigir, etc...
+
+    #No geral funciona muito bem, mas tem suas falhas, ela é propensa a erros, que perduram muito.
+    #Retroativamente alterada, novas emoções e memoria podem recontextualziar as memórias antigas.
+    #Afetada pelas emoçoções, seja na quantidade de detalhes devido o contexto ou a falta.
+    #Tudo porque nada disso tem alta resolução, é uma aproximação
+
+    #Isso exige consitencia nos layouts, simbolos, simplicidade.
+
+#Reconhecer x Lembrar
+    #É mais fácil reconhecer do que lembrar.
+    #Reocnhecer está ligado a usar caminhos familiares para recordar de algo.
+    #Por isso há métodos para ajudar a lembrar usando apenas reocnhecimento, como exemplo tópicos em uma apresentação.
+    #
