bff579e14ac86c44abcdd69f1625a3616fdc5880
Higor <higor.freitas@usp.br>
2026-03-16 20:39:03 -0300

    BD I

---
+++ BD II/Aula6.sql
@@ -0,0 +1,13 @@
+#Generalização e especialização
+#Análogo a disjunção de BD I
+#Pode ter heranças mútiplas, do mesmo jeito que pode ter disjunção múltipla
+    #Muito complexo a modelagem prática
+#O t é de total, ou seja, ou é um ou outro, ou seja, não pode ser ambos nem nenhum.
+
+
+#Resumo no slide
+    #A maioria das coisas é similar a BD 1
+    #Entidade associativa, agregação
+    #Diferentes representações
+
+
