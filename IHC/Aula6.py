3134bbdc94d7c95c5d06e168570eddcff9f87db3
Higor <higor.freitas@usp.br>
2026-03-16 22:04:17 -0300

    IHC

---
+++ IHC/Aula6.py
@@ -0,0 +1,38 @@
+#Aspectos Cognitivos
+
+    #Aspectos mais amplos da cognição que afetam a interação, como memória, atenção, aprendizagem, etc...
+
+
+#Aprendizagem
+    #Experiencia é o melhor método
+    #Novo é dificil, ja aprendido é facil
+    #Aprende-se mais rapido quando se pratica e o "vocabulario" condiz com a tarefa
+    #Generalizar aprendizados é uma característica muito boa do ser humano
+    #Situações muito complexas, que possuem muitas variáveis, possuem curvas de aprendizado muito lentas
+
+    #Nem sempre o usuario irá pelo caminho mais fácil para realizar uma tarefa, é muito comum ele seguir o mesmo
+        #Existe resitênica
+
+    #Isso tudo deve ser levado em consideração na hora de criar sistemas interativos.
+        #Fato de intepretabilidade, o sistema deve ser fácil de entender.
+
+    #Quando não há risco envolvido no sistema, é mais provavel ele explorar e aprender mais
+        #É necessário indicar pogrsso, dar feedback para o cliente
+        #Fale sobre as ações de amneira explicita
+
+
+#Resolução de problema
+    #Existem os problemas reflexivos, que necessitam de mais tempo para serem realizados
+    #Todos os problemas começam sendo reflexivos
+    
+
+
+#Leitura fala e escrita
+    #Linguagem escrita é permanente e fixa, escuta é transiente
+    #Ler pode ser mais fácil
+    #Existem pessoas com necessidades especiais
+
+
+#Modelo mental
+    #Fluxo que seguimos para realizar uma tarefa nova através das novas experiencias, um método criado e idealizado
+    #Ex Carro diferente, ajustar temperatura
