4e922e00cec942b3ad536bf5a737dffe44847be7
Higor <higor.freitas@usp.br>
2026-03-25 22:08:19 -0300

    BD

---
+++ BD II/Aula8.sql
@@ -0,0 +1,19 @@
+#Auto-Relacionamentos
+    #Basciamente uma relação entre entres de uma emesma entidade
+    
+
+
+#Também dtem as generealizações e especializações
+    #Bascaimente uma entidade mais genérica que tem outras entidades mais específicas
+    #Exemplo: Funcionario é uma entidade genérica, e Gerente e Vendedor
+        #Pode ser feito simplesmente juntando as tabelas, 
+        #Pode ser obrigatorio ou não
+        #Pode ser exclusivo ou não
+
+
+#Campos redudantes não são bons, só com justificativas de desempenho
+    #Ex no avião,ter o numero de reservas, invés de ficar fazendo levantamento de cada registro da tabela de reservas
+
+
+#Relacionamento mutualmente exclusivos
+    #
