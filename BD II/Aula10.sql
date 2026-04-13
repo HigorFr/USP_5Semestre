dead758dc39cabef333bf7207d8fe4ca1afc2cfa
Higor <higor.freitas@usp.br>
2026-04-13 20:34:04 -0300

    BD I

---
+++ BD II/Aula10.sql
@@ -0,0 +1,12 @@
+#Ultimas aulas o prof faltou
+
+
+#Normalização
+    #semalhante ao feito em BD I
+    #Evitar anomalias, como redudancia, que pode gerar inconsistencias
+        #Ambos os dados nesse caso estão certos semânticamente, mas não é útil
+
+
+    #O algotimo para normalziar em sí não mudam nada.
+
+    #sempre decompondo tabeals para evitar redundância
