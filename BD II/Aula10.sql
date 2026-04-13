619a3eecea9c899731f1a1f5b01e2b9eeda3a77d
Higor <higor.freitas@usp.br>
2026-04-13 20:34:51 -0300

    Bd

--- BD II/Aula10.sql
+++ BD II/Aula10.sql
@@ -9,4 +9,8 @@
 
     #O algotimo para normalziar em sí não mudam nada.
 
+    #segunda forma tira redundancia parcial, aquela que tem dependencias funcionais parciais.
+    #terceira forma tira dependencias transitivas, ou seja, quando um atributo depende de outro que não é chave
+    #?quarta forma tira dependencias multivaloradas, ou seja, quando um atributo depende de outro que não é chave, mas tem mais de um valor
+
     #sempre decompondo tabeals para evitar redundância
