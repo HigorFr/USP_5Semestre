31d37b007328bb2ba74ceee57d48567e3d624622
Higor <higor.freitas@usp.br>
2026-06-22 21:37:16 -0300

    IHC

---
+++ IHC/Aula11.py
@@ -0,0 +1,45 @@
+#Desing Thinking
+    #Uma das técnicas para estruturar dados 
+
+
+
+
+#Desing é uma abordagem centrada no usuário para a resolução de problemas e desenvolvimento de soluções inovadoras. Ele envolve a compreensão profunda das necessidades e desejos dos usuários, a geração de ideias criativas, a prototipagem rápida e a iteração constante com base no feedback dos usuários. 
+# O Design Thinking é amplamente utilizado em diversas áreas, incluindo design de produtos, serviços, experiências e processos, promovendo a colaboração interdisciplinar e a empatia com os usuários finais.
+    #Estética e funcionalidade.
+
+#Empatia, conceito utilizado, para se colocar no lugar do usuário.
+
+#DT não é uma metodologia empirica, mas uma abordagem que pode ser aplicada em diferentes contextos e adaptada às necessidades específicas de cada projeto. Ele não segue um conjunto rígido de etapas, mas sim um processo iterativo que envolve a exploração, a experimentação e a aprendizagem contínua.
+
+
+#imersão, Definição, Ideação, Prototipagem, Teste, Implementação depois itera nisso.
+#Várias etapas, utiliza entregáveis na definição. Como o proprio mapa de empatia, persona, cenários, etc... 
+#Todas essas ferramentam se relaciona, e é necessário entender como aplicar cada uma delas.
+
+#Exemplo de cenário com iteração de cadastro de projetos.
+#Fato: Manual, descreve o hoje, e é avaliado os problemas.
+#Realiza varios cenários diferentes e propoe, exemplo: Cenário A - invés da secretaria ter que cadastrar, é o proprio professor.
+#Cenario B - Alunos cadastra. É necessário confimação  do professor.
+
+#Cada cenários tem todo passo a passo, de como seria a interação, e o que aconteceria em cada etapa. Cada um é avaliado de acordo com o negócio.
+
+
+#Prototipagem
+#Então cria-se representaçãos, com 
+#Em alta fidelidade, cores, checkbox, lugar para texto, ficam de maneira que o produto final tenha pouca diferença. (Figma, fluid, proto IO, vairios exemplos no slide)
+#Os em baixa fidelidade é mais rápido, para testar coisas mais simples, wireframes facilitam, nada concretizado, só estrutura. ()
+
+    #*Segunda etapa do projeto da disciplina é necessario usar alguma dessas ferramentas.
+
+
+
+
+
+
+
+
+
+
+
+
